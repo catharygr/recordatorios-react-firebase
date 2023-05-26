69c901d1694b09118de58ec788fbcf1c53c310ff
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-26 13:03:16 +0100

    Trabajando en el componnete Home y css

--- src/componentes/NuevoRecordatorio.jsx
+++ src/componentes/NuevoRecordatorio.jsx
@@ -10,19 +10,28 @@ export default function NuevoRecordatorio({ manejarVisibilidad }) {
         <input id="titulo" type="text" placeholder="Nuevo recordatorio" />
         <label htmlFor="nota">Nota</label>
         <textarea placeholder="Text area" id="nota" cols="3" />
-        <label htmlFor="fecha">Fecha</label>
-        <input type="date" id="fecha" />
-        <label htmlFor="hora">Hora</label>
-        <input id="hora" type="time" />
-        <label htmlFor="seleccionar-lista">Seleccionar lista</label>
-        <select name="" id="seleccionar-lista">
-          <option>Supermecado</option>
-          <option>Supermecado</option>
-          <option>Supermecado</option>
-          <option>Supermecado</option>
-        </select>
-        <label htmlFor="marcado">Marcado</label>
-        <input id="marcado" type="checkbox" checked />
+        <div className="form-elemento-container">
+          <label htmlFor="fecha">Fecha</label>
+          <input type="date" id="fecha" />
+        </div>
+        <div className="form-elemento-container">
+          <label htmlFor="hora">Hora</label>
+          <input id="hora" type="time" />
+        </div>
+        <div className="form-elemento-container">
+          <label htmlFor="seleccionar-lista">Seleccionar lista</label>
+          <select name="" id="seleccionar-lista">
+            <option>Supermecado</option>
+            <option>Supermecado</option>
+            <option>Supermecado</option>
+            <option>Supermecado</option>
+          </select>
+        </div>
+        <div className="form-elemento-container">
+          <label htmlFor="marcado">Marcado</label>
+          <input id="marcado" type="checkbox" />
+        </div>
+
         <label htmlFor="cargar-imagen">Cargar imagen</label>
         <input id="cargar-imagen" type="file" />
       </form>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 export default function Home() {
   return (
     <main className="home-container">
-      <div className="tablero">Holaa</div>
-      <section className="home-lista-container">Holaa</section>
-      {/* <footer className="home-footer">
-        <button>Lista nueva</button>
-        <button>Recordatorio nuevo</button>
-      </footer> */}
+      <div className="tablero">
+        <div className="tablero-tarjeta">
+          <div className="tablero-tarjeta--primera-fila">
+            <img
+              className="tablero-tarjeta--img"
+              src="/assest/reloj.png"
+              alt="Icono de reloj"
+            />
+            <p className="tablero-tarjeta--num">5</p>
+          </div>
+          <p className="tablero-tarjeta--fecha">fecha: 26/07/23</p>
+        </div>
+      </div>
+      <section className="home-lista-container"></section>
     </main>
   );
 }
