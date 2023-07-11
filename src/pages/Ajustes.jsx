5c8ed853fbfd2584d6c9055aa3be3384b5f54ceb
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-11 11:58:19 +0100

    Trabajando en la autentificación y css

--- src/pages/Ajustes.jsx
+++ src/pages/Ajustes.jsx
@@ -1,9 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+import { auth } from "../scripts/firebase";
+import { signOut } from "firebase/auth";
+
 export default function Ajustes() {
+  const navigate = useNavigate();
+  function handleSalir() {
+    signOut(auth)
+      .then(() => navigate("/loguear"))
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
   return (
     <div className="ajustes-container">
       <button>Modo claro</button>
-      <button className="salir-btn">Salir</button>
+      <button onClick={handleSalir} className="salir-btn">
+        Salir
+      </button>
     </div>
   );
 }
--- src/pages/Loguear.jsx
+++ src/pages/Loguear.jsx
@@ -1,10 +1,29 @@
 import { useId } from "react";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../scripts/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Loguear() {
   const id = useId();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    signInWithEmailAndPassword(auth, form.email, form.password)
+      .then(() => navigate("/"))
+      .catch((error) => {
+        setError(error.message);
+      });
+    setForm({
+      email: "",
+      password: "",
+    });
   }
 
   return (
@@ -12,14 +31,23 @@ export default function Loguear() {
       <h1>Loguear</h1>
       <form className="loguear-form" onSubmit={handleSubmit}>
         <label htmlFor={`${id}-email`}>Email</label>
-        <input type="email" id={`${id}-email`} placeholder="Rellene su email" />
+        <input
+          type="email"
+          id={`${id}-email`}
+          placeholder="Rellene su email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
         <label htmlFor={`${id}-password`}>Contraseña</label>
         <input
           type="password"
           id={`${id}-password`}
           placeholder="Rellene su password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
         />
         <button type="submit">Ingresar</button>
+        {error && <p className="loguear-form_error">{error}</p>}
       </form>
     </div>
   );
