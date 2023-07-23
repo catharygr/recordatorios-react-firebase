54ce2224a07b28919f083898db75a9ba9ba39647
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-23 16:18:50 +0100

    Ultimando la lógica

--- src/componentes/BarraHeader.jsx
+++ src/componentes/BarraHeader.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import { Settings, Home, List } from "react-feather";
+import { MisTipoHomeContext } from "../scripts/DataContext";
+import { useContext } from "react";
 
 export default function BarraHeader() {
+  const { setHomeTipo } = useContext(MisTipoHomeContext);
   return (
     <header className="header-container">
       <Link to="editar-lista">
         <List size={18} />
       </Link>
-      <Link className="header-inicio" to="/">
+      <Link
+        onClick={() => setHomeTipo("listas")}
+        className="header-inicio"
+        to="/"
+      >
         <Home size={22} color="var(--color-green)" />
       </Link>
       <Link className="header-ajustes" to="ajustes">
